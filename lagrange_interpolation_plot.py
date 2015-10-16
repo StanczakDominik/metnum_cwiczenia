a4eb156f64a32ba206042ce9c7ef0b9cc4f78490
Dominik Stańczak <stanczakdominik@gmail.com>
2015-10-16 10:25:39 +0200

    neville interpolation

--- lagrange_interpolation_plot.py
+++ lagrange_interpolation_plot.py
@@ -4,15 +4,20 @@ import matplotlib.pyplot as plt
 points = np.loadtxt("lagrange_interpolation_dane")
 fit = np.loadtxt("lagrange_interpolation_fit")
 
-print(points)
-print(fit)
-
 points_x = points[:,0]
 points_y = points[:,1]
 
 fit_x = fit[:,0]
 fit_y = fit[:,1]
 
-plt.plot(points_x, points_y, "bo")
-plt.plot(fit_x, fit_y, "g-")
+plt.plot(points_x, points_y, "bo", label = "Data")
+plt.plot(fit_x, fit_y, "g-.", lw=5, label = "Lagrange")
+
+fit = np.loadtxt("neville_interpolation_fit")
+fit_x = fit[:,0]
+fit_y = fit[:,1]
+plt.plot(fit_x, fit_y, "r--", lw=5, label = "Neville")
+
+plt.grid()
+plt.legend()
 plt.show()
---
+++ neville_interpolation.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+double L(double x_val, vector<double>& xi, vector<double>& yi)
+{
+    vector<double> P = yi;
+    for (unsigned int i = 1 ; i<xi.size(); i++)
+    {
+        for (unsigned int j=0; j < yi.size()-i; j ++)
+        {
+            P[j]=((x_val - xi[i+j])*P[j]-(x_val-xi[j])*P[j+1])/(xi[j]-xi[i+j]);
+        }
+    }
+    return P[0];
+} 
+
+void load_data(string file_name, vector<double>& x_points, vector<double>& y_points)
+{
+    ifstream data_file(file_name);
+    while(true)
+    {
+        double x;
+        double y;
+        data_file >> x;
+        data_file >> y;   
+        if (data_file.eof())
+        {
+            return;
+        }
+        x_points.push_back(x);
+        y_points.push_back(y);     
+    }
+}
+
+double min(vector<double> x)
+{
+    double result = x[0];
+    for (unsigned int i=1; i<x.size(); ++i)
+    {
+        if (x[i] < result)
+        {
+            result = x[i];
+        }
+    }
+    return result;
+}
+double max(vector<double> x)
+{
+    double result = x[0];
+    for (unsigned int i=1; i<x.size(); ++i)
+    {
+        if (x[i] > result)
+        {
+            result = x[i];
+        }
+    }
+    return result;
+}
+int main(int argc, char** argv)
+{
+    cout << setiosflags(ios::fixed) << setprecision(12) << endl;
+        
+    vector<double> xi;
+    vector<double> yi;
+    
+    load_data(argv[1], xi, yi);
+    
+    vector<double> x;
+    vector<double> y;
+    for (double i =min(xi); i<= max(xi); i+=(max(xi)-min(xi))/10000.)
+    {
+        x.push_back(i);
+    }    
+    
+    for (auto x_val: x)
+    {   
+        double Lx = L(x_val, xi, yi);
+        y.push_back(Lx);
+    }
+    
+    ofstream fit_file(argv[2]);
+    
+    for (unsigned int i=0; i<x.size(); i++)
+    {
+        fit_file << x[i] << "\t" << y[i] << endl;
+    }
+}
