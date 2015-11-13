31abfd07955e7ba07f5be078cdd4bcd507d278f2
Dominik Stanczak <stanczakdominik@gmail.com>
2015-11-13 10:50:23 +0100

    rng histogram on doubles

--- rng.cpp
+++ rng.cpp
@@ -10,15 +10,15 @@ int N = 1000000;
 int N_bins = 1024;
 int rng()
 {
-    int a = 33;
-    int c = 1;
-    int m = 1024;
+    int a = 1103515245;
+    int c = 12345;
+    int m = (int)1024;
 	static int x = 0;
 	x = (a*x+c) % m;
 	return x;
 }
 
-vector<unsigned int> histogram(vector<int> dane, unsigned int N, int x_min = 0, int x_max = 1024)
+vector<unsigned int> histogram(vector<double> dane, unsigned int N, int x_min = 0, int x_max = 1)
 {
     vector<unsigned int> v(N,0);
     double dx = (double)(x_max-x_min)/(double)N;
@@ -35,19 +35,16 @@ vector<unsigned int> histogram(vector<int> dane, unsigned int N, int x_min = 0, 
 
 int main()
 {
-    vector<int> dane;
+    vector<double> dane;
     
     for (int i=0; i<N; i++)
     {   
-        dane.push_back(rng());
+        dane.push_back(random()/(double)RAND_MAX);
     }
     vector<unsigned int> hist = histogram(dane, N_bins);
     for (auto val: hist)
     {
         cout << val << endl;
     }
-    
-    int i = 0;
-    
-    for (int i = 3;i<15;i*=2
+   
 }
